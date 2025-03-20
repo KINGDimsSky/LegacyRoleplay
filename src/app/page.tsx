21c0b0ca9f9593dd063bef23806d7b5eacffe5c8
KINGDimsSky <dimasmarcelo2004@gmail.com>
2025-03-20 15:50:21 +0700

    Repairing Structure And Other

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <div className="">
-      <MaxWidthWrapper>
+    <div className="flex flex-col min-h-screen bg-yellow-200">
+      <MaxWidthWrapper className="min-h-screen">
         <h2>Hello World</h2>
       </MaxWidthWrapper>
     </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,14 @@
+import Image from "next/image";
+import MaxWidthWrapper from "../MaxWidthWrapper";
 import { Button } from "../ui/button";
 
 export default function Navbar() {
   return (
-    <div className="">
-        <Button variant={"destructive"} className="cursor-pointer p-6">Click Me</Button>
+    <div className="fixed top-0 w-full">
+      <MaxWidthWrapper className="flex justify-between items-center  bg-green-300 p-4 rounded-lg mt-4">
+        <Image src={"/icons/LPRP.png"} alt="LegacyRoleplay" width={20} height={20}/>
+        <Button variant={"destructive"} className="cursor-pointer p-2">Click Me</Button>
+      </MaxWidthWrapper>
     </div>
   );
 }
