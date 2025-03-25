b0accb9293a5066b9f78cfed276da9229bf61a64
KINGDimsSky <dimasmarcelo2004@gmail.com>
2025-03-25 14:45:22 +0700

    Adding Credit Section

--- src/components/Credit.tsx
+++ src/components/Credit.tsx
@@ -1,8 +1,19 @@
+import { PenTool } from "lucide-react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Button } from "./ui/button";
 
 export default function CreditSection () {
     return (
         <MaxWidthWrapper>
+          <div className="flex w-full justify-center gap-4">
+            <Button className="flex items-center p-12 rounded-full">
+              <PenTool className="" width={20} height={20}/>
+              <div className="flex flex-col">
+                <p className="text-2xl font-semibold">High Quality</p>
+                <p className="text-xs w-48 bg-emerald-200">Dengan maintenace Rutin bertujuan meningkatkan Experience pemain</p>
+              </div>
+            </Button>
+          </div>
           <h2 className="text-white text-3xl">Testing</h2>
         </MaxWidthWrapper>
     )
